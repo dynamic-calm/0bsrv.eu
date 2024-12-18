000f5605f069c40b73dbe1ef9bda00d69d6c7382
Mateo Presa Castro <mateopresacastro@gmail.com>
2024-12-19 00:13:57 +0100

    fix: link

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="mt-2 flex h-10 w-full items-center justify-center border-t border-gray-700 bg-gray-200 px-2 py-2 font-mono text-xs text-gray-900">
       <Link
-        href="https://github.com/mateopresa/0bsrv.eu"
+        href="https://github.com/mateopresacastro/0bsrv.eu"
         className="w-fit"
         target="_blank"
       >
